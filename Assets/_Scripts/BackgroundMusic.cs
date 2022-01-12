d3bd5e10ef96e04a11f7364c61391b87acd27b44
Ruben Ohnmacht <26636673+shniqq@users.noreply.github.com>
2022-01-13 00:45:04 +0100

    Added background music.

---
+++ Assets/_Scripts/BackgroundMusic.cs
@@ -0,0 +1,17 @@
+using DG.Tweening;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] _backgroundAudioClips;
+    [SerializeField] private AudioSource _backgroundMusicSource;
+    
+    
+    private void Awake()
+    {
+        _backgroundMusicSource.clip = _backgroundAudioClips[Random.Range(0, _backgroundAudioClips.Length)];
+        _backgroundMusicSource.DOFade(1f, 1f).ChangeStartValue(0f).SetEase(Ease.InQuad);
+        _backgroundMusicSource.Play();
+    }
+}
--- Assets/_Scripts/Card.cs
+++ Assets/_Scripts/Card.cs
@@ -46,18 +46,44 @@ public class Card : MonoBehaviour
     {
         DOTween.Sequence()
             .Insert(0f, transform.DOMoveY(_position.y, duration * 0.6f).SetEase(Ease.OutBack))
+            .InsertCallback(0f, () =>
+            {
+                if (_index == 0)
+                {
+                    PlayRandomCardSound();
+                }
+            })
             .Insert(duration * 0.6f, transform.DOMoveX(_position.x, duration * 0.3f).SetEase(Ease.Linear))
-            .Insert(duration * 0.6f, transform.DOMoveZ(_position.z, duration * 0.3f).SetEase(Ease.Linear));
+            .Insert(duration * 0.6f, transform.DOMoveZ(_position.z, duration * 0.3f).SetEase(Ease.Linear))
+            .InsertCallback(duration * 0.6f, () =>
+            {
+                if (_index == 0)
+                {
+                    Invoke(nameof(PlayRandomCardSound), 0.1f);
+                    Invoke(nameof(PlayRandomCardSound), 0.2f);
+                    Invoke(nameof(PlayRandomCardSound), 0.3f);
+                    Invoke(nameof(PlayRandomCardSound), 0.4f);
+                    Invoke(nameof(PlayRandomCardSound), 0.5f);
+                    Invoke(nameof(PlayRandomCardSound), 0.6f);
+                    Invoke(nameof(PlayRandomCardSound), 0.7f);
+                    Invoke(nameof(PlayRandomCardSound), 0.8f);
+                }
+            });
     }
 
     public void PlayCard()
     {
         _cardManager.OnCardPlayed(this);
 
-        _audioSource.PlayOneShot(_cardPlayAudioClips[Random.Range(0, _cardPlayAudioClips.Length)]);
+        PlayRandomCardSound();
         DOTween.Sequence().SetEase(_cardPlayEase)
             .Insert(0f, transform.DORotate(new Vector3(0, 180, 0), _cardPlayDuration, RotateMode.WorldAxisAdd))
             .Insert(0f, transform.DOMoveX(-transform.position.x, _cardPlayDuration))
             .InsertCallback(0.5f, () => _sortingGroup.sortingOrder = _index);
+    }
+
+    private void PlayRandomCardSound()
+    {
+        _audioSource.PlayOneShot(_cardPlayAudioClips[Random.Range(0, _cardPlayAudioClips.Length)]);
     }
 }
