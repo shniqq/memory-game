feabece24c93b979131d5574fefc9b1ec0072c30
Ruben Ohnmacht <26636673+shniqq@users.noreply.github.com>
2022-01-12 23:46:42 +0100

    Fixed background rendering on iOS.

--- Assets/_Scripts/Background.cs
+++ Assets/_Scripts/Background.cs
@@ -5,18 +5,18 @@ using Random = UnityEngine.Random;
 public class Background : MonoBehaviour
 {
     [SerializeField] private Sprite[] _backgroundSprites;
+    [SerializeField] private SpriteRenderer _patternSpriteRenderer;
     [SerializeField] private SpriteRenderer _backgroundSpriteRenderer;
-    [SerializeField] private Camera _camera;
     [SerializeField, Range(0, 1)] private float _alpha;
 
     [Inject] private IColorProvider _colorProvider;
-    
+
     private void Awake()
     {
-        _backgroundSpriteRenderer.sprite = _backgroundSprites[Random.Range(0, _backgroundSprites.Length)];
+        _patternSpriteRenderer.sprite = _backgroundSprites[Random.Range(0, _backgroundSprites.Length)];
         var backgroundColor = _colorProvider.GetBackgroundColor();
-        backgroundColor.a = _alpha;
         _backgroundSpriteRenderer.color = backgroundColor;
-        _camera.backgroundColor = backgroundColor;
+        backgroundColor.a = _alpha;
+        _patternSpriteRenderer.color = backgroundColor;
     }
 }
