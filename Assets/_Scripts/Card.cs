58d1c97bb604a4f39a158aabe109afa05d7971fb
Ruben Ohnmacht <26636673+shniqq@users.noreply.github.com>
2022-01-13 00:01:43 +0100

    Adds card play sounds.

--- Assets/_Scripts/Card.cs
+++ Assets/_Scripts/Card.cs
@@ -10,7 +10,8 @@ public class Card : MonoBehaviour
     [SerializeField] private Ease _cardPlayEase;
     [SerializeField] private SpriteRenderer _frontBackground;
     [SerializeField] private SpriteRenderer _icon;
-    
+    [SerializeField] private AudioClip[] _cardPlayAudioClips;
+    [SerializeField] private AudioSource _audioSource;
 
     [Inject]
     private int Index
@@ -35,7 +36,7 @@ public class Card : MonoBehaviour
         var cardConfig = _cardConfigProvider.GetConfig();
         Id = cardConfig.Item1;
         _icon.sprite = cardConfig.Item2;
-        
+
         transform.position = Vector3.down * 30f + Vector3.back * 10f;
 
         _frontBackground.color = _cardColorProvider.GetCardColor(_index);
@@ -52,6 +53,8 @@ public class Card : MonoBehaviour
     public void PlayCard()
     {
         _cardManager.OnCardPlayed(this);
+
+        _audioSource.PlayOneShot(_cardPlayAudioClips[Random.Range(0, _cardPlayAudioClips.Length)]);
         DOTween.Sequence().SetEase(_cardPlayEase)
             .Insert(0f, transform.DORotate(new Vector3(0, 180, 0), _cardPlayDuration, RotateMode.WorldAxisAdd))
             .Insert(0f, transform.DOMoveX(-transform.position.x, _cardPlayDuration))
